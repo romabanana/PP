07bc5d7cbaccfeb1bd3586d7b891378547b6256b
romabanana <roman4uni@gmail.com>
2025-10-03 10:14:27 -0300

    working of ft

--- main.py
+++ main.py
@@ -49,10 +49,12 @@ class GameState:
         self.turn = 1
         self.hp = 100
         self.mana = 10
-        self.seats = 100
         self.depth = 0
         self.player_name = None
+        self.focus = None
         self.country = "Argentina"
+        self.focus_array = [0]*100
+        self.focus_cost_array = []
 
 
 
@@ -130,79 +132,124 @@ def get_depth(node) -> int:
         node = node.parent
     return depth
 
-def build_tree_from_json(tree, data, parent=None):
+def build_tree_from_json(self, tree, data, count=0,parent=None):
     parent = parent or tree.root
+    
     for key, value in data.items():
-        node = parent.add(key, data=value if value else None)
+
+        count += 1
+        if self.app.game_state.focus_array[count] == 1:
+            node = parent.add(f"[green]{key}[/green]", data=value if value else None)
+            node.expand()
+        elif self.app.game_state.focus_array[count] == 2:
+            node = parent.add(f"[red]{key}[/red]", data=value if value else None)
+
+        else:
+            node = parent.add(key, data=value if value else None)
+        
+
         if isinstance(value, dict) and value:
-            build_tree_from_json(tree, value, node)
+            build_tree_from_json(self, tree, value, count, node)
+
+# def set_tree_state(self, tree, data, parent= None):
 
 class PolicyScene(Screen):
     # CSS_PATH = "css/PolicyScene.css"
 
-
-        
     # print(events[0]["effects"]["economy"])  
     def compose(self) -> ComposeResult:
         focuses = json.loads(Path("focus/focus_data.json").read_text())["focuses"]
-    
-        with Container(id="top-bar"):
-           yield Static(f"  {self.app.game_state.hp}", id="hp")
-           yield Static(f"  {self.app.game_state.mana}", id="mana")
-           yield Static("")
-           yield Static("23/02/24")
         
+        self.topbar = TopBar()
+        yield self.topbar
+
         with Container(id="grid-container-policy"):
             data = json.loads(Path("focus/focus.json").read_text())
             tree = Tree("Focus Tree", id="focus")  # root node
-            # tree.show_root = True
+            tree.show_root = False
             
             tree.ICON_NODE = ''
             tree.ICON_NODE_EXPANDED = ''
 
-            # tree.auto_expand = False
+            tree.auto_expand = False
 
             tree.guide_depth = 2
-
+            self.app.game_state.focus_array[0] = 1
+            self.app.game_state.focus = tree.root
             # tree.add_json(data)
-            build_tree_from_json(tree, data)
-
+            build_tree_from_json(self, tree, data)
+            # tree_set_state(self, tree, data)
+            
             tree.root.expand()
             test = tree.root.children[0]
             test.expand()
             # test.remove()
             
             yield tree
-            yield Static(focuses[self.app.game_state.depth]["desc"], id="policy-desc")
+            
+            yield Static(focuses[self.app.game_state.focus.id]["desc"], id="policy-desc")
+            yield Static(str(focuses[self.app.game_state.focus.id]["cost"]), classes="box", id="focus-cost")
             yield Static(f"{self.app.game_state.depth}", classes="box", id="debug1")
+            
+            yield Button("TAKE", classes="box", id="focus-button")
             yield Button("COMPLETE", classes="box", id="exit")
 
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
+        focuses = json.loads(Path("focus/focus_data.json").read_text())["focuses"]
+    
         if event.button.id == "exit":
+            self.log(self.app.game_state.depth)
+            self.log(self.app.game_state.focus_array)
             self.app.push_screen(GameScene())
+        elif event.button.id == "focus-button":
+            if self.app.game_state.focus_array[self.app.game_state.focus.id] == 0:
+                if self.app.game_state.mana >= focuses[self.app.game_state.focus.id]["cost"]: 
+                    self.app.game_state.focus_array[self.app.game_state.depth] = 1
+                    self.app.game_state.focus.expand()
+                    self.app.game_state.mana -= focuses[self.app.game_state.focus.id]["cost"]
+                    self.app.game_state.focus.set_label(f"[green]{self.app.game_state.focus.label}[/green]")
+                    for id in focuses[self.app.game_state.focus.id]["excludes"]:
+                        for sib in self.app.game_state.focus.siblings:
+                            if sib.id == id:
+                                sib.set_label(f"[red]{sib.label}[/red]")
+                                self.app.game_state.focus_array[sib.id] = 2
+                    self.topbar.update()
+
     
 
-
-    def on_tree_node_selected(self, event: Tree.NodeExpanded) -> None:
+    def on_tree_node_selected(self, event: Tree.NodeSelected) -> None:
         focuses = json.loads(Path("focus/focus_data.json").read_text())["focuses"]
-
-        node = event.node
-
-        self.app.game_state.depth = node.id
+        self.app.game_state.focus = event.node
+       
+        self.app.game_state.depth = self.app.game_state.focus.id
         self.query_one("#debug1", Static).update(f"{self.app.game_state.depth}")
-        self.query_one("#policy-desc", Static).update(focuses[self.app.game_state.depth]["desc"])
-        if self.app.game_state.depth == 7:
-            node.collapse()
-        if self.app.game_state.depth == 3:
-            node.collapse()
+        
+        self.query_one("#policy-desc", Static).update(focuses[self.app.game_state.focus.id]["desc"])
+        self.query_one("#focus-cost", Static).update(str(focuses[self.app.game_state.focus.id]["cost"]))
+       
 
-        # parent_data = node.parent
-        # if self.app.game_state.depth % 2 == 0:
-        #     if self.app.game_state.mana <= parent_data.get("cost"):
-        #         node.collapse()
-                
-        # else:
+    def on_tree_node_collapsed(self, event: Tree.NodeCollapsed) -> None:
+        if self.app.game_state.focus_array[self.app.game_state.focus.id] == 1:
+            event.node.expand()
+       
+
+    def on_tree_node_expanded(self, event: Tree.NodeExpanded) -> None:
+        focuses = json.loads(Path("focus/focus_data.json").read_text())["focuses"]
+            
+        self.app.game_state.focus = event.node
+        id = self.app.game_state.focus.id
+
+        self.app.game_state.depth = self.app.game_state.focus.id
+        self.query_one("#debug1", Static).update(f"{self.app.game_state.depth}")
+        
+        self.query_one("#policy-desc", Static).update(focuses[self.app.game_state.focus.id]["desc"])
+        self.query_one("#focus-cost", Static).update(str(focuses[self.app.game_state.focus.id]["cost"]))
+        
+        if self.app.game_state.focus_array[id] == 0:
+            event.node.collapse()
+        
+        
           
             
 
